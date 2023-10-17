36f4c8ba8fa447d03f6f70c5cb10dd9b37b72e8e
Aldaine-Legion <vainedev@gmail.com>
2023-10-16 20:10:30 -0500

    created helpers for status code and Json response

---
+++ src/helpers/JsonResponse.helper.ts
@@ -0,0 +1,24 @@
+/**
+ * @author Aldaine Clarke <github.com/aldaineclarke>
+ */
+
+import { Response } from "express";
+import HttpStatusCode from "./StatusCodes.helper";
+
+export class JsonResponse{
+    public static success(res:Response, data: object | object[], message = "Request was successful", status_code: HttpStatusCode.OK){
+        return res.status(status_code).json({
+            message,
+            data,
+            errors: []
+        });
+    }
+    public static error(res:Response, message="Request was unsuccessful", status_code: HttpStatusCode.BAD_REQUEST, errors: object[] | string[]){
+        return res.status(status_code).json({
+            message,
+            data : [],
+            errors
+        });
+
+    }
+}
