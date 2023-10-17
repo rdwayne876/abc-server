b2f65e7a3ebca2a152c52923fcbf7ed88967f710
Aldaine-Legion <vainedev@gmail.com>
2023-10-16 20:44:33 -0500

    Added JSDocs to helper for JsonResponse

--- src/helpers/JsonResponse.helper.ts
+++ src/helpers/JsonResponse.helper.ts
@@ -6,14 +6,31 @@ import { Response } from "express";
 import HttpStatusCode from "./StatusCodes.helper";
 
 export class JsonResponse{
-    public static success(res:Response, data: object | object[], message = "Request was successful", status_code = HttpStatusCode.OK){
+    /**
+     * 
+     * @param {Response} res This is the response from the middleware passed
+     * @param {string} message Readable message to caller
+     * @param {object} data This is an optional object which holds data to present to caller
+     * @param {HttpStatusCode} status_code  This is a enum for the status code of the response
+     * @returns 
+     */
+    public static success(res:Response, message = "Request was successful", data: object = {}, status_code = HttpStatusCode.OK){
         return res.status(status_code).json({
             message,
             data,
             errors: []
         });
     }
-    public static error(res:Response, message="Request was unsuccessful",  errors: object[] | string[],status_code = HttpStatusCode.BAD_REQUEST){
+
+    /**
+     * 
+     * @param {Response} res This is the response from the middleware passed
+     * @param {string} message Readable message to caller
+     * @param {string[] | object[]} errors This is an optional object or string array which holds errors to present to caller
+     * @param {HttpStatusCode} status_code  This is a enum for the status code of the response
+     * @returns 
+     */
+    public static error(res:Response, message="Request was unsuccessful",  errors: object[] | string[] = [],status_code = HttpStatusCode.BAD_REQUEST){
         return res.status(status_code).json({
             message,
             data : [],
