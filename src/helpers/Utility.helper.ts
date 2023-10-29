fbebc5b8625372252e101ceda0f8798c8f4083fd
Aldaine-Legion <vainedev@gmail.com>
2023-10-29 14:57:34 -0500

    updated utility function

--- src/helpers/Utility.helper.ts
+++ src/helpers/Utility.helper.ts
@@ -1,7 +1,16 @@
 export const getKeyByValue = <T,U>(map:Map<T, U>, value: U)=>{
     for(let [key, val] of map.entries()){
         if(value === val ){
+            console.log(value)
             return key;
         }
     }
+}
+
+export const transformMapToObject = <T,U>(map:Map<T,U>)=>{
+    let transformedObj:{[s:string]: U} = {};
+    for(let [key, val] of map.entries()){
+        transformedObj[`${key}`] = val;
+    }
+    return transformedObj;
 }
